5fd502deab9d271aa994a12185db53744934d7dd
JeanColonia <engshka123@gmail.com>
2022-05-13 01:44:14 -0500

    addng components, sharedmodule & header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './components/shared/material/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/shared/material/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatIconModule
+
+  ],
+  exports: [MatIconModule]
+})
+export class SharedModule { }
