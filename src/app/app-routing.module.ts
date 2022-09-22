7c1b9fa2e24ce5b5bbb944498875ba5bee0e7677
JeanColonia <engshka123@gmail.com>
2022-09-22 17:23:30 -0500

    in company / courses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from './components/about/about.component'
 import { InicioComponent } from './components/inicio/inicio.component';
-import {HeaderComponent} from  './components/header/header.component';
-
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { IncompanyComponent } from './components/incompany/incompany.component';
 const routes: Routes = [
+  
   {
-    path: 'about', component: AboutComponent
+    path: 'nosotros', component: AboutComponent
+  },
+  {
+    path: 'servicios', component: ServiciosComponent
+  },
+  {
+    path: 'cursos', component: CursosComponent
+  },
+  {
+    path: 'incompany', component: IncompanyComponent
   },
   {
     path: '', component: InicioComponent, pathMatch:'full'}, ];
--- src/app/components/incompany/incompany.component.ts
+++ src/app/components/incompany/incompany.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-incompany',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./incompany.component.css']
 })
 export class IncompanyComponent implements OnInit {
-
-  constructor() { }
-
+  imgsCarouselCompany: any[] = [
+    { id: 1, img: '../../../../assets/incompany/capacitacion-in-house-1.png' },
+    { id: 2, img: '../../../../assets/incompany/capacitacion-in-house-2.png' }
+  ];
+  constructor(private _config: NgbCarouselConfig) {
+    _config.interval=2700,
+    _config.pauseOnHover=true,
+    _config.showNavigationArrows=false
+  }
   ngOnInit(): void {
   }
 
